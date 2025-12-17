f163cc7eedebc1f26494847d9e03a9e30fcc3ed3
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-17 22:07:24 +0500

    add balance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import Balance from "./Balance";
+
 function App() {
   return (
       <div>
         <h1>Finance Tracker</h1>
+        <Balance/>
       </div>
   )
 }
---
+++ src/Balance.jsx
@@ -0,0 +1,10 @@
+function Balance() {
+    return(
+        <div>
+            <h2>Balance</h2>
+        </div>
+    )
+    
+}
+
+export default Balance
