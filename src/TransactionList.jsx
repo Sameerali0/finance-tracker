72d918c96394a7030b665ba39d1745955cb95b94
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-20 12:25:53 +0500

    Update TransactionList to show transactions or message if empty

--- src/TransactionList.jsx
+++ src/TransactionList.jsx
@@ -2,6 +2,15 @@ function TransactionList({transactions}) {
     return(
         <div>
             <h2>Transactions</h2>
+
+            {transactions.length === 0 && <p>No transactions here</p> }
+            <ul>
+                {transactions.map((item)=>(
+                    <li key={item.id}>
+                        {item.text} --- {item.amount} --- ({item.type})
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
