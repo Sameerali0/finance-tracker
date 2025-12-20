1bde6df7fe8e8da1cda56a757051b19b24b3c35d
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-20 12:15:13 +0500

    Update Balance component to calculate income and expenses

--- src/Balance.jsx
+++ src/Balance.jsx
@@ -1,7 +1,23 @@
 function Balance({ transactions }) {
+
+    const income= transactions
+      .filter((trans)=> trans.type === "income")
+      .reduce((total, trans) => total + trans.amount, 0)
+
+    const expense= transactions
+      .filter((trans) =>trans.type === "expense")
+      .reduce((total, trans) =>total + trans.amount, 0)
+
+    const balance= income - expense
+
     return(
         <div>
-            <h2>Balance</h2>
+
+          <h2>Balance</h2>
+          <p>Total: {balance}</p>
+          <p>Income: {income}</p>
+          <p>Expense: {expense}</p>
+
         </div>
     )
     
