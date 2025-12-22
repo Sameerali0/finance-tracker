0c702725e2daa6753369bee3212c5f71f8e4d2ca
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-22 12:24:58 +0500

    added lacalStorage with useEffect hook for data save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Balance from "./Balance";
 import TransactionForm from "./TransactionForm";
 import TransactionList from "./TransactionList";
 
 function App() {
 
-  const [transactions, setTransactions] = useState([])
+  const [transactions, setTransactions] = useState(() =>{
+
+      const saved = localStorage.getItem("transactions")
+      return saved ? JSON.parse(saved):[];
+  })
+
+  useEffect(()=>{
+      localStorage.setItem("transactions", JSON.stringify(transactions))
+  }, [transactions])
 
   return (
       <div className="main-container">
