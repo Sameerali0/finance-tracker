d1e992abe520893f3386adcdee54666e805a818a
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-24 13:14:19 +0500

    added pages folder with Welcome.jsx component

---
+++ src/pages/Welcome.jsx
@@ -0,0 +1,12 @@
+function Welcome(){
+
+    return(
+        <div>
+            <h1>Finance Tracker</h1>
+            <p>Track your income and expences easily</p>
+            <button>Start</button>
+        </div>
+    )
+}
+
+export default Welcome
