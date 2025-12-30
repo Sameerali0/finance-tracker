6b41e36e8db0dafd97ddb7e4c9194c107c145c3f
Sameerali0 <sameer.ali.professional@gmail.com>
2025-12-30 10:40:04 +0500

    added welcome page styling

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -5,10 +5,12 @@ function Welcome(){
     const goTo= useNavigate()
 
     return(
-        <div>
-            <h1>Finance Tracker</h1>
-            <p>Track your income and expences easily</p>
-            <button onClick={()=> goTo("/loading")}>Start</button>
+        <div className="welcome-container">
+            <div className="welcome-card">
+                <h1>Finance Tracker</h1>
+                <p>Track your income and expences easily</p>
+                <button onClick={()=> goTo("/loading")}>Start</button>
+            </div>    
         </div>
     )
 }
