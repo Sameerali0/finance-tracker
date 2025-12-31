4cda1132ee4ef249a60d01115a63358af15f77f7
Sameerali0 <sameer.ali.professional@gmail.com>
2026-01-01 01:46:55 +0500

    working on list styling

--- src/TransactionList.jsx
+++ src/TransactionList.jsx
@@ -1,18 +1,28 @@
 function TransactionList({transactions, deleteTransaction, editingTransaction}) {
     return(
-        <div>
-            <h2>Transactions</h2>
+        <div className="transaction-list-container">
+            <h2 className="transaction-heading">Transactions</h2>
 
-            {transactions.length === 0 && <p>No transactions here</p> }
-            <ul>
+            {transactions.length === 0 ? (<p className="no-transacitons">No transactions here</p>): (
+            <ul className="transaction-list">
+                <li className="transaction-list-header">
+                    <span>Title</span>
+                    <span>Amount</span>
+                    <span>Type</span>
+                    <span>Category</span>
+                    <span>Date</span>
+                    <span>Actions</span>
+                </li>
+
                 {transactions.map((item)=>(
                     <li key={item.id} className={item.type === "income" ? "income" : "expense"}>
                         {item.text} - Rs {item.amount} - ({item.type}) ({item.category}) - {item.date}
-                        <button onClick={()=> editingTransaction(item)}>edit</button>
-                        <button onClick={()=> deleteTransaction(item.id)}>del</button>
+                            <button className="edit-btn" onClick={()=> editingTransaction(item)}>Edit</button>
+                            <button className="delete-btn" onClick={()=> deleteTransaction(item.id)}>Delete</button>
                     </li>
                 ))}
             </ul>
+            )}
         </div>
     )
 }
